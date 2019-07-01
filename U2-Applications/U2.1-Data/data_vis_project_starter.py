4657ec9dcf0a16090e9853180a7ba4770ffdb04e
AJV1416 <jocelynalondravera@gmail.com>
2019-07-01 14:51:14 -0700

    data project

--- U2-Applications/U2.1-Data/data_vis_project_starter.py
+++ U2-Applications/U2.1-Data/data_vis_project_starter.py
@@ -8,13 +8,47 @@ import json
 from textblob import TextBlob
 import matplotlib.pyplot as plt
 
+def avg(lst):
+    return sum(lst)/len(lst)
+
+
+
 #Get the JSON data
 tweetFile = open("TwitterData/tweets_small.json", "r")
 tweetData = json.load(tweetFile)
 tweetFile.close()
 
 # Continue your program below! 
+tweettext = []
+tweetstring = ""
+for tweet in tweetData:
+    tweetstring += tweet["text"]
+    #blob = TextBlob(tweet['text'])
+    #tweettext.append(blob)
+
+
+#my_list = [1,2,3,4,5,6] 
+#print(sum(my_list)/len(my_list))
+
+tweetBlob = TextBlob(tweetstring)
+
+blob_poplarity = []
+for blob in tweettext:
+    blob_polarity.append(blob.polarity)
+
+blob_subjectivity = []
+for blob in tweettext:
+    blob_subjectivity.append(blob_subjectivity)
+
+word_dict = {}
+
+for word in tweetBlob.words:
+    word_dict[word.lower()] = tweetBlob.word_counts[word.lower()]
+
+print(word_dict)
+
+#blob_polarity.sort()
+#print(blob_polarity)
 
 # Textblob sample:
-tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
+tb = TextBlob("You are a horrible computer scientist.")
